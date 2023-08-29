d4bfdfc82e7007ffd555f0bfe9dae18a4cdb8c87
fede-sol <fedesolanes1@gmail.com>
2023-08-29 18:14:45 -0300

    Propiedades para la base de datos remota

--- src/main/java/com/example/demo/SistemaDeGestionDeReclamosApplication.java
+++ src/main/java/com/example/demo/SistemaDeGestionDeReclamosApplication.java
@@ -2,12 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SistemaDeGestionDeReclamosApplication {
 
+
+	
 	public static void main(String[] args) {
-		SpringApplication.run(SistemaDeGestionDeReclamosApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SistemaDeGestionDeReclamosApplication.class, args);
+		
+		EjemploController ejemploController = context.getBean(EjemploController.class);
+        ejemploController.realizarConsulta();
+
+		context.close();
+		
+
 	}
 
 }
