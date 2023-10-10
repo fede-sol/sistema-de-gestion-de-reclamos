fcdd3480df209c700604bf64dbe9132716a917e8
fede-sol <fedesolanes1@gmail.com>
2023-10-10 16:46:08 -0300

    Configurados los controller

--- src/main/java/com/example/controladores/controladores_rest/RestControllerEdificio.java
+++ src/main/java/com/example/controladores/controladores_rest/RestControllerEdificio.java
@@ -1,5 +1,17 @@
 package com.example.controladores.controladores_rest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.controladores.Controlador;
+
+@RestController
+@RequestMapping("/unidad")
+@CrossOrigin
 public class RestControllerEdificio {
-    
+
+    @Autowired
+	Controlador controlador;
 }
+
--- src/main/java/com/example/controladores/controladores_rest/RestControllerPersona.java
+++ src/main/java/com/example/controladores/controladores_rest/RestControllerPersona.java
@@ -1,5 +1,18 @@
 package com.example.controladores.controladores_rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.controladores.Controlador;
+
+@RestController
+@RequestMapping("/unidad")
+@CrossOrigin
 public class RestControllerPersona {
 
+
+    @Autowired
+	Controlador controlador;
 }
