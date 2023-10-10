d8b196b176cfeca3ea975f7da812c4932dd23eda
fede-sol <fedesolanes1@gmail.com>
2023-10-10 16:24:04 -0300

    Cambios en unidad exception

--- src/main/java/com/example/exceptions/UnidadException.java
+++ src/main/java/com/example/exceptions/UnidadException.java
@@ -1,6 +1,10 @@
 package com.example.exceptions;
 
-public class UnidadException extends Exception {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UnidadException extends RuntimeException {
 
 	private static final long serialVersionUID = -1690698840733203643L;
 
